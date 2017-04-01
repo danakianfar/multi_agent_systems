87b77715093f82cdef3c37b814eb88d6457adf13
marco <federicimarcosx@gmail.com>
2017-04-01 20:14:32 +0200

    yet another addition for the submission

--- Project/Python/run_simulation.py
+++ Project/Python/run_simulation.py
@@ -10,5 +10,5 @@ simulation = Simulation(MainBus)
 
 
 simulation.reset()
-simulation.execute(iterations=whole_day, animate=True, interval=250, save_file='simulation_result.avi')
 plt.show()
+simulation.execute(iterations=whole_day, animate=True, interval=250, save_file='simulation_result.avi')
