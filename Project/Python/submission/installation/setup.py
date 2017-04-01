4ee5279cab5de28e954815e1f5a09852b5f01b19
Dana Kianfar <dana.kianfar@gmail.com>
2017-04-01 20:55:27 +0200

    submission

--- Project/Python/submission/installation/setup.py
+++ Project/Python/submission/installation/setup.py
@@ -2,7 +2,7 @@
 # Install script for Windows 64-bit
 # 1. Right-click on cmd.exe and run as administrator
 # 2. Navigate to the folder containing this file
-# 3. run `python setup.py`
+# 3. run `python -m setup.py`
 #####################################################
 import pip
 
@@ -23,26 +23,26 @@ if __name__ == '__main__':
 
     # pip install   
     dependencies = ['numpy', 'pandas', \
-                    'tensorflow', 'keras', 'flask', \
+                    'jupyter', 'keras', \
                     'matplotlib', 'scipy', 'pyparsing']
 
     for pkg in dependencies:
         install(pkg)
 
-    import glob
-    import shutil, errno
+    # import glob
+    # import shutil, errno
 
-    # Copy NetLogo extensions to directory
-    extensions = glob.glob(r'.\*')
+    # # Copy NetLogo extensions to directory
+    # extensions = glob.glob(r'.\*')
 
-    nl_path = r'C:\Program Files\NetLogo*'
-    nl_path = glob.glob(nl_path)[0]
-    nl_extensions_dir = nl_path + r'\app\extensions'
+    # nl_path = r'C:\Program Files\NetLogo*'
+    # nl_path = glob.glob(nl_path)[0]
+    # nl_extensions_dir = nl_path + r'\app\extensions'
 
-    print(extensions)
-    print(nl_extensions_dir) 
+    # print(extensions)
+    # print(nl_extensions_dir) 
 
-    for extension in extensions:
-        # print(extension.replace(r'.\', ''))
-        print(extension)
-        copy_dir(extension, nl_extensions_dir + extension)
+    # for extension in extensions:
+    #     # print(extension.replace(r'.\', ''))
+    #     print(extension)
+    #     copy_dir(extension, nl_extensions_dir + extension)
